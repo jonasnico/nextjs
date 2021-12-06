628c98cbdd86b3a3e8f3f22ab813002d775c9849
jonasnico <70371917+jonasnico@users.noreply.github.com>
2021-12-06 23:14:13 +0100

    Added new components, global styles, css utils,

---
+++ pages/music/index.js
@@ -0,0 +1,22 @@
+import Layout, { siteTitle } from '/pages/components/layout'
+
+
+export default function MusicList() {
+    return (
+        
+        <Layout>
+
+            <h1>Music List</h1>
+
+            <ul>
+                <li>Teste</li>
+                <li>Testings</li>
+                
+                
+                </ul>
+        </Layout>
+
+        
+    ) 
+
+}
