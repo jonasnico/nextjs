d84e0d11f24313331f22561fa2e66ad95018cf46
jonasnico <70371917+jonasnico@users.noreply.github.com>
2021-12-07 03:10:58 +0100

    functions to fetch external data and render.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '/pages/components/layout'
-import utilStyles from './/styles/utils.module.css'
+import utilStyles from '/styles/utils.module.css'
+import { getSortedPostsData } from './/lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
 
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,7 +25,20 @@ export default function Home() {
           (Link to{' '}
           <a href="https://github.com/jonasnico" target="_blank" rel="noreferrer">My Github</a>.)
         </p>
-        
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
